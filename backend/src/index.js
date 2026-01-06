0b1dcf8fc47bccb7a2b2b06329e5ef89f0cf1472
BzTheRipper <bayzid.baytur.redwan87@gmail.com>
2026-01-06 16:58:33 +0600

    navigation completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ const createChatAppSchema = require("./models/createChatAppSchema.js");
 const createUserTable = require("./models/user.model.js");
 const cookieParser = require("cookie-parser");
 const createMessageShcema = require("./models/message.model.js");
+const cors = require("cors");
 
 const app = express();
 
@@ -14,6 +15,11 @@ const PORT = process.env.DB_PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    
+}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes)
